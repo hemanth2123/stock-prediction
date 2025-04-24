d41a393473d09d3477251800c58daa9187f10e17
hemanth2123 <legendimmortal123@gmail.com>
2025-04-24 21:58:14 +0530

    Added Reliance to stock list and updated HTML dropdown

--- app.py
+++ app.py
@@ -1,40 +1,33 @@
 from flask import Flask, render_template, request
-import yfinance as yf
-from sklearn.linear_model import LinearRegression
-import pandas as pd
+from model import predict_next_day  # ⬅ Import the function
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+stocks = {
+    "IRCTC": "IRCTC.NS",
+    "Tata Motors": "TATAMOTORS.NS",
+    "IRFC": "IRFC.NS",
+    "BEL": "BEL.NS",
+    "HAL": "HAL.NS",
+    "Mazagon Dock": "MAZDOCK.NS",
+    "RVNL": "RVNL.NS",
+    "Titagarh Wagons": "TWL.NS",
+    "Vedanta": "VEDL.NS",
+    "Reliance": "RELIANCE.NS"  # ✅ Added
+}
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    prediction = None
+    predicted_date = None
+    selected_symbol = None
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    ticker = request.form['ticker']
+    if request.method == 'POST':
+        selected_symbol = request.form['symbol']
+        prediction, predicted_date = predict_next_day(selected_symbol)
 
-    # Download latest data
-    stock = yf.download(ticker, start="2020-01-01")
-
-    # Create target column
-    stock['Target'] = stock['Close'].shift(-1)
-    stock.dropna(inplace=True)
-
-    X = stock[['Open', 'High', 'Low', 'Close', 'Volume']]
-    y = stock['Target']
-
-    # Train/Test Split
-    X_train = X[:-1]
-    y_train = y[:-1]
-
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-
-    # Predict next day using last row
-    last_row = X.iloc[[-1]]
-    predicted_price = model.predict(last_row)[0]
-
-    return render_template('result.html', prediction=round(predicted_price, 2), ticker=ticker)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template("index.html", stocks=stocks,
+                           prediction=prediction,
+                           predicted_date=predicted_date,
+                           selected_symbol=selected_symbol)
